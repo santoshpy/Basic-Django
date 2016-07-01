bac4fdbeee28056e26fe825cd6916ea4988eff8d
santoshpurbey <purbey.santosh@hotmail.com>
2016-07-01 13:15:32 +0545

    Created BlogPost into models.py and registered into admin.py

--- blog_Pro/blog_app/models.py
+++ blog_Pro/blog_app/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class PostBlog(models.Model):
+    title = models.CharField(max_length=140)
+    content = models.TextField()
+    author = models.CharField('full Name', max_length=60)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    
