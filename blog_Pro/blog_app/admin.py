94707c1bcefed5c9f896909ec7ea726a80993e9c
santoshpurbey <purbey.santosh@hotmail.com>
2016-07-01 18:41:44 +0545

    Edited Model admin with fields and list_display attribute

--- blog_Pro/blog_app/admin.py
+++ blog_Pro/blog_app/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import PostBlog
 
-admin.site.register(PostBlog)
+class PostBlogAdmin(admin.ModelAdmin):
+    fields = ['author', 'title', 'content'] 
+    list_display=['title', 'content', 'created', 'updated']
+    
+
+    class Meta:
+    	model = PostBlog
+    
+
+admin.site.register(PostBlog, PostBlogAdmin)
--- blog_Pro/blog_app/models.py
+++ blog_Pro/blog_app/models.py
@@ -5,9 +5,9 @@ from django.db import models
 # Create your models here.
 
 class PostBlog(models.Model):
-    title = models.CharField(max_length=140)
-    content = models.TextField()
-    author = models.CharField('full Name', max_length=60)
+    title = models.CharField(help_text='write beautiful title of your blog', max_length=140)
+    content = models.TextField(help_text='Write something you wish to write!')
+    author = models.CharField(help_text='full Name', max_length=60)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
