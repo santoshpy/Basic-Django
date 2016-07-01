5379c7f4ae671a90935a45c769001878976729de
santoshpurbey <purbey.santosh@hotmail.com>
2016-07-01 19:21:46 +0545

    Some feture added in admin pannel like list_filter, search_fields,list_editable , odering

--- blog_Pro/blog_app/admin.py
+++ blog_Pro/blog_app/admin.py
@@ -5,8 +5,19 @@ from .models import PostBlog
 
 class PostBlogAdmin(admin.ModelAdmin):
     fields = ['author', 'title', 'content'] 
-    list_display=['title', 'content', 'created', 'updated']
-    
+    list_display = ['title', 'author', 'created', 'updated']
+    list_display_links = ['created', 'updated']
+    list_filter = ['author']
+    search_fields = ['title', 'content']
+    list_editable = ['title']
+    ordering = ['author']
+    list_select_related = True
+
+    #actions_on_top = False 
+    #actions_on_bottom = False
+	#actions_selection_counter =  True
+
+
 
     class Meta:
     	model = PostBlog
